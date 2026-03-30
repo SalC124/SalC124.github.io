fe604146e6c0fc14e6b075725dcdc5f9cc6d707c
SalC124 <Saltcal124@gmail.com>
2026-03-29 21:27:40 -0400

    footer stays at the bottom like a footer should, and grid is set to the center of the page

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -5,27 +5,30 @@ import { cards } from "../data/cards";
 const Home = () => {
   return (
     <>
-      <div class={styles.header}>
-        <h1>
-          <a href="https://BCAr.fun/">BCAr.fun</a>
-          {" Link Bank, "}
-          <em>but better</em>
-        </h1>
-      </div>
-      <CardGrid cards={cards} />
-      <div class={styles.footer}>
-        <p>
-          Based on <a href="http://bcar.fun/">BCAr.fun</a>
-          {" by "}
-          <a href="http://aidanglickman.com/">Aidan Glickman</a>
-        </p>
+      <div class={styles.container}>
+        <div class={styles.header}>
+          <h1>BCA Link Bank</h1>
+        </div>
+        <div class={styles.gridWrapper}>
+          <CardGrid cards={cards} />
+        </div>
+        <div class={styles.footer}>
+          <p>
+            Based on <a href="http://bcar.fun/">BCAr.fun</a>
+            {" by "}
+            <a href="http://aidanglickman.com/">Aidan Glickman</a>
+          </p>
 
-        <p>
-          <em>
-            Made Better by Sal (
-            <a href="https://github.com/SalC124">https://github.com/SalC124</a>)
-          </em>
-        </p>
+          <p>
+            <em>
+              Made Better by Sal (
+              <a href="https://github.com/SalC124">
+                https://github.com/SalC124
+              </a>
+              )
+            </em>
+          </p>
+        </div>
       </div>
     </>
   );
